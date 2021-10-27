9453044e5f8ecba6321ba540a86358e98a7cb966
Alba Mendez <me@alba.sh>
2021-10-27 03:19:20 +0200

    vector table (code boots!!)

--- entry.c
+++ entry.c
@@ -1,7 +1,34 @@
+#include "constants.h"
+
+struct vector_table {
+    void *initial_sp;
+    void *reset;
+    void *nmi;
+    void *hard_fault;
+    void *memory_management_fault;
+    void *bus_fault;
+    void *usage_fault;
+    void *__reserved_1[4];
+    void *sv_call;
+    void *__reserved_debug;
+    void *__reserved_2;
+    void *pend_sv;
+    void *systick;
+    void *irq[240];
+};
+
+static void _start();
+
+// must be the first definition in this file
+const struct vector_table __VECTOR_TABLE __attribute__((section("VECTOR_TABLE"))) = {
+    .initial_sp = (void*)(BASE_SRAM1 + SIZE_SRAM1),
+    .reset = _start,
+    // rest zeroed for now
+};
+
 void main();
 
-// must be the first defined function in the file
-void _start() {
+static void _start() {
     main();
     // control should never reach back here,
     // but if it does, idle loop
